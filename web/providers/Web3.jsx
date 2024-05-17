c482aef548c3169f21caa28c15cb921a64df1328
lxj <1851816672@qq.com>
2024-05-18 04:35:53 +0800

    []change network

--- web/providers/Web3.jsx
+++ web/providers/Web3.jsx
@@ -2,7 +2,7 @@
  * @Author: lxj 1851816672@qq.com
  * @Date: 2023-12-24 05:11:34
  * @LastEditors: lxj 1851816672@qq.com
- * @LastEditTime: 2024-05-08 00:20:27
+ * @LastEditTime: 2024-05-18 04:34:13
  * @FilePath: /marketPlace/providers/Web3.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,6 +12,7 @@ import { configureChains, createConfig, WagmiConfig, } from "wagmi"
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit"
 import {
   sepolia,
+  scrollTestnet,
   goerli,
   mainnet,
   optimism,
@@ -20,11 +21,11 @@ import {
   bsc
 } from "wagmi/chains"
 import { publicProvider } from "wagmi/providers/public"
-
+console.log(sepolia, scrollTestnet);
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
-    sepolia,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : [])
+    scrollTestnet,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [scrollTestnet] : [])
   ],
   [publicProvider()]
 )
