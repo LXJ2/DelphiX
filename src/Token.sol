9f80f72459f9f576c916b92e10f4c0842a7b543b
0xy0ung <35057221+0xy0ung@users.noreply.github.com>
2024-05-19 01:05:24 +0800

    update HackathonProject

--- src/Token.sol
+++ src/Token.sol
@@ -5,16 +5,16 @@ import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract Token is ERC20, Ownable{
 
-    constructor() ERC20("Tether USD", "USDT")Ownable(msg.sender){
+    constructor() ERC20("Test USD", "USDT")Ownable(msg.sender){
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 
-    function mint(address to, uint256 amount) public onlyOwner{
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
     
-    function burn(address from, uint256 amount) public onlyOwner{
+    function burn(address from, uint256 amount) public {
         _burn(from, amount);
     }
-    
+
 }
--- src/hackathon.sol
+++ src/hackathon.sol
@@ -10,7 +10,7 @@ contract HackathonProject is Ihackathon{
     address[] public adminList;
     mapping (string =>string) public winner;
     mapping (string =>mapping(string=> uint256)) votes;
-    mapping (address =>mapping(string=> uint256)) stakeNum;
+    mapping (address =>mapping(string=> mapping(string=> uint256))) public stakeNum;
     mapping (string =>uint256) trackAmount;
     mapping (address =>mapping(string=> uint256)) votedTrack;
     mapping (address => mapping(string=>string[])) votedTeam;
@@ -53,12 +53,12 @@ contract HackathonProject is Ihackathon{
         usdt = _usdt;
     }
 
-    function stake(string memory track,string memory _team,uint256 amount) external returns(bool){
+    function stake(string memory track,string memory _team, uint256 amount) external returns(bool){
         require(ENDTIME-block.timestamp>STOPTIME);
         require(IERC20(usdt).allowance(msg.sender,address(this))>0);
         require(IERC20(usdt).balanceOf(msg.sender)>0);
         IERC20(usdt).transferFrom(msg.sender,address(this),amount);
-        stakeNum[msg.sender][track] +=amount;
+        stakeNum[msg.sender][track][_team] +=amount;
         trackAmount[track] +=amount;
         // Check if the voteData is not already in the array
         bool alreadyVoted = false;
@@ -70,10 +70,8 @@ contract HackathonProject is Ihackathon{
         }
 
         // If not already voted, add the voteData
-        if (!alreadyVoted) {
-            votedTeam[msg.sender][track].push(_team);
-            votes[track][_team] += amount;
-        }
+        votedTeam[msg.sender][track].push(_team);
+        votes[track][_team] += amount;
 
         emit Stake(msg.sender,amount);
         emit Vote(_team,track,msg.sender);
@@ -81,6 +79,7 @@ contract HackathonProject is Ihackathon{
     }
 
     function setHackathonResult(string memory track,string memory _winner) external returns(bool){
+        require(isAdmin(msg.sender));
         require(block.timestamp>ENDTIME);
         winner[track] = _winner;
         return true;
@@ -88,19 +87,18 @@ contract HackathonProject is Ihackathon{
 
     function claim (string memory track) external{
         require(block.timestamp>ENDTIME);
-        require(isStaked(track,msg.sender),"please stake first");
         require(votedTeam[msg.sender][track].length > 0, "you have not voted");
 
         uint256 amount = settleWinnerPrice(track, msg.sender);
         address payable _address = payable(msg.sender);
-        stakeNum[msg.sender][track] = 0;
+        stakeNum[msg.sender][track][winner[track]] = 0;
         IERC20(usdt).transfer(_address, amount);       
         
     }
 
     function settleWinnerPrice(string memory track, address user) public view returns (uint256) {
         require(block.timestamp > ENDTIME, "Voting period has not ended");
-        require(isStaked(track, msg.sender), "Please stake first");
+        require(isStaked(track, msg.sender, winner[track]), "Please stake first");
 
         bool isWinner = false;
         for (uint256 i = 0; i < votedTeam[msg.sender][track].length; i++) {
@@ -112,8 +110,9 @@ contract HackathonProject is Ihackathon{
         require(isWinner, "you are not the winner");
 
         uint256 _trackAmount = getTrackAmount(track);
-        uint256 _stakeAmount = stakeNum[user][track];
-        uint256 _voteAmount = votes[track][winner[track]]; // Assuming _track and _team are not defined elsewhere
+        uint256 _stakeAmount = stakeNum[user][track][winner[track]];
+
+        uint256 _voteAmount = votes[track][winner[track]]; 
 
         // Calculate the winning amount
         uint256 winningAmount = (_stakeAmount * _trackAmount) / _voteAmount;
@@ -121,8 +120,19 @@ contract HackathonProject is Ihackathon{
         return winningAmount;
     }
 
-    function isStaked(string memory track,address user) internal view returns(bool) {
-        return stakeNum[user][track] > 0;
+    function isStaked(string memory track,address user,string memory _team) internal view returns(bool) {
+        return stakeNum[user][track][_team] > 0;
+    }
+
+    function isAdmin(address user)internal view returns(bool) {
+        bool admin = false;
+        for (uint256 i = 0; i < adminList.length; i++) {
+            if (adminList[i] == user) {
+                admin = true;
+                break;
+            }
+        }
+        return admin;
     }
 
     function getWinner(string memory track) external view returns(string memory){
@@ -145,8 +155,11 @@ contract HackathonProject is Ihackathon{
         return adminList;
     }
 
+    function getVoteNum(string memory track, string memory _team) external view returns(uint256){
+        return votes[track][_team];
+    }
 
-    function getStakeNum(string memory track, address user) external view returns(uint256){
-        return stakeNum[user][track];
+    function getStakeNum(string memory track, address user, string memory _team) external view returns(uint256){
+        return stakeNum[user][track][_team];
     }
 }
