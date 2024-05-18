57b9142f1f447540d995d5c751aed3a143f6adf3
lxj <1851816672@qq.com>
2024-05-18 16:13:16 +0800

    []

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -2,19 +2,19 @@
  * @Author: lxj 1851816672@qq.com
  * @Date: 2024-05-18 03:30:59
  * @LastEditors: lxj 1851816672@qq.com
- * @LastEditTime: 2024-05-18 11:54:42
+ * @LastEditTime: 2024-05-18 16:11:26
  * @FilePath: /DelphiX/web/pages/index.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React from 'react'
-import {Layout, Menu, Card, Row, Col, Typography, Button, InputNumber} from 'antd'
-import {useEffect, useState} from 'react'
+import { Layout, Menu, Card, Row, Col, Typography, Button, InputNumber } from 'antd'
+import { useEffect, useState } from 'react'
 // import logo from "@/public/logo.png"
 import banner from 'public/banner.png'
 import Image from "next/image"
 
-const {Header, Content} = Layout
-const {Title, Paragraph, Text} = Typography
+const { Header, Content } = Layout
+const { Title, Paragraph, Text } = Typography
 
 const ETHBeijing = () => {
   const [currentTab, setCurrentTab] = useState(1)
@@ -25,27 +25,55 @@ const ETHBeijing = () => {
   const [poolAmount, setPoolAmount] = useState(321)
 
 
-  const tabs = [{index: 1, name: 'ETH Beijing'}, {index: 2, name: 'Upcoming Games'}]
+  const tabs = [{ index: 1, name: 'ETH Beijing' }, { index: 2, name: 'Upcoming Games' }]
+  const [teams, setTeams] = useState([
+    {
+      id: 1, poolAmount: 0, inputValue: 0, projectName: 'DelphiX', intro: `DelphiX is a web3 forecast market for developers competition focusing on hackathon as well as coder events.
+    It provides on chain staking to win the betting pool for layer 1 & layer 2 completition. Now it supports ETH Beijing as season 1.` },
+    {
+      id: 2, poolAmount: 321, inputValue: 0, projectName: 'Forest3', intro: `Forest3 is a blockchain-based decentralized application that allows users to set personal goals, 
+    deposit self-discipline funds, and distribute bonuses according to the voting results and completion status.` }
+  ]);
+
+  // Handler to update item value
+  const onInputChange = (id, newValue) => {
+    setTeams(prevItems =>
+      prevItems.map(item =>
+        item.id === id ? { ...item, inputValue: newValue } : item
+      )
+    );
+  };
 
   const tabClick = (data) => {
     setCurrentTab(data.index)
   }
 
-  const [value, setValue] = useState(0)
-
-  const onDecrease = () => {
+  const onDecrease = (id, value) => {
     if (value == 0) return
-    setValue(prevValue => prevValue - 1)
+    setTeams(prevItems =>
+      prevItems.map(item =>
+        item.id === id ? { ...item, inputValue: value - 1 } : item
+      )
+    );
+
   }
 
-  const onIncrease = () => {
-    setValue(prevValue => prevValue + 1)
+  const onIncrease = (id, value) => {
+    setTeams(prevItems =>
+      prevItems.map(item =>
+        item.id === id ? { ...item, inputValue: value + 1 } : item
+      )
+    );
+  }
+
+  const bet = () => {
+
   }
 
   return (
-    <Layout style={{backgroundColor: '#fff', borderRadius: '10px'}}>
+    <Layout style={{ backgroundColor: '#fff', borderRadius: '10px' }}>
       <Header>
-        <Menu style={{fontSize:'20px'}} theme="light" mode="horizontal" defaultSelectedKeys={[1]}>
+        <Menu style={{ fontSize: '20px' }} theme="light" mode="horizontal" defaultSelectedKeys={[1]}>
           {tabs.map(item => {
             return <Menu.Item key={item.index} onClick={() => {
               tabClick(item)
@@ -53,14 +81,14 @@ const ETHBeijing = () => {
           })}
         </Menu>
       </Header>
-      <Content style={{padding: '20px', backgroundColor: '#fff'}}>
-        {currentTab == 1 ? <div style={{background: '#f0f2f5', padding: '20px 0', borderRadius: '10px'}}>
+      <Content style={{ padding: '20px', backgroundColor: '#fff' }}>
+        {currentTab == 1 ? <div style={{ background: '#f0f2f5', padding: '20px 0', borderRadius: '10px' }}>
 
-          <div style={{textAlign: 'center', background: '#e9e9e9', marginBottom: '20px'}}>
-            <Image src={banner} alt="image"/>
+          <div style={{ textAlign: 'center', background: '#e9e9e9', marginBottom: '20px' }}>
+            <Image src={banner} alt="image" />
           </div>
-          <div style={{padding: '0 50px'}}>
-            <section style={{marginBottom: '40px'}}>
+          <div style={{ padding: '0 50px' }}>
+            <section style={{ marginBottom: '40px' }}>
               <Title level={2}>Intro & Rules</Title>
               <Paragraph>
                 ETH Beijing is supported by ETH Foundation and Scroll to help Web3 coders in Ethereum to bring more ideas and grow...
@@ -69,7 +97,7 @@ const ETHBeijing = () => {
                 DelphiX is to help users who focus on ETH Beijing with interest to participate betting "Who Is Winner". ETH Beijing Winner can get the grant of the 20% pool and bets users can get 80% if they bet the right result.
               </Paragraph>
             </section>
-            <section style={{marginBottom: '40px'}}>
+            <section style={{ marginBottom: '40px' }}>
               <Title level={2}>Statistics</Title>
               <Paragraph>Competition Ends in {competitionEndTime}</Paragraph>
               <Paragraph>Place Bets Ends in {betsEndsTime}</Paragraph>
@@ -83,33 +111,34 @@ const ETHBeijing = () => {
             <section>
               <Title level={2}>Teams</Title>
               <Row gutter={16}>
-                <Col span={8}>
-                  <Card title="DelphiX" bordered={false}>
-                    <Paragraph>Intro</Paragraph>
-                    <Paragraph>DelphiX is a web3 forecast market for developers competition focusing on hackathon as well as coder events.
-                      It provides on chain staking to win the betting pool for layer 1 & layer 2 completition. Now it supports ETH Beijing as season 1.
-                    </Paragraph>
-                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                      <Paragraph> Pool</Paragraph>
-                      <Paragraph> {poolAmount} USDT</Paragraph>
-                    </div>
-
-                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                      <div style={{display: 'flex'}}>
-                        <Button onClick={onDecrease}>-</Button>
-                        <InputNumber
-                          min={0}
-                          value={value}
-                          addonAfter="USDT"
-                          onChange={setValue}
-                        />
-                        <Button onClick={onIncrease}>+</Button>
+                {teams.map(item => {
+                  return <Col span={8} key={item.id}>
+                    <Card title={item.projectName} bordered={false}>
+                      <Paragraph>Intro</Paragraph>
+                      <Paragraph>
+                        {item.intro}
+                      </Paragraph>
+                      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <Paragraph> Pool</Paragraph>
+                        <Paragraph> {item.poolAmount} USDT</Paragraph>
                       </div>
-                      <Button>Bets</Button>
-                    </div>
+                      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <div style={{ display: 'flex' }}>
+                          <Button onClick={() => onDecrease(item.id, item.inputValue)}>-</Button>
+                          <InputNumber
+                            min={0}
+                            value={item.inputValue}
+                            addonAfter="USDT"
+                            onChange={(newValue) => onInputChange(item.id, newValue)}
+                          />
+                          <Button onClick={() => onIncrease(item.id, item.inputValue)}>+</Button>
+                        </div>
+                        <Button>Bets</Button>
+                      </div>
+                    </Card>
+                  </Col>
+                })}
 
-                  </Card>
-                </Col>
               </Row>
             </section>
           </div>
