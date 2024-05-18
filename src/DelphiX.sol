266bb673a60736a1bd78f2569d8d50ca6de2ea65
0xy0ung <35057221+0xy0ung@users.noreply.github.com>
2024-05-18 11:49:40 +0800

    add remappint

--- src/DelphiX.sol
+++ src/DelphiX.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract DelphiX {
     address public owner;
