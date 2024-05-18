8999b09435e117d839d90529e598632d96c68f80
lxj <1851816672@qq.com>
2024-05-18 11:44:55 +0800

    [update] add unite

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -2,7 +2,7 @@
  * @Author: lxj 1851816672@qq.com
  * @Date: 2024-05-18 03:30:59
  * @LastEditors: lxj 1851816672@qq.com
- * @LastEditTime: 2024-05-18 11:38:24
+ * @LastEditTime: 2024-05-18 11:43:12
  * @FilePath: /DelphiX/web/pages/index.jsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -94,6 +94,7 @@ const ETHBeijing = () => {
                         <InputNumber
                           min={0}
                           value={value}
+                          addonAfter="USDT"
                           onChange={setValue}
                         />
                         <Button onClick={onIncrease} >+</Button>
